b132c23bb0c5c2a0a680267fa548082fc3c3782f
Jacob Handley <jacobhandley50@gmail.com>
2018-09-05 01:01:15 -0400

    Updated

--- project1/project1.cc
+++ project1/project1.cc
@@ -7,9 +7,14 @@ int main(){
 	int n;
 	cout<<"Enter an integer between 2 and 30,000 as your upper bound."<<endl;
 	cin>>n;
+	//Check if input is within bounds
 	if(n < 2 || n > 30000){
 		cout<<"Out of range."<<endl;
 		return 0;
 	}
+	//If the user inputs a non-integer value, it will be truncated, and if no
+	//input is given, the program will not continue, so the "Nan" and "Missing
+	//Argument" messages are unnecessary.
+
 	return 0;
 }
