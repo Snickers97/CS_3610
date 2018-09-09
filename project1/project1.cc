35ac6d034c734e9c0fcaf4c161430592136f77c9
Jacob Handley <jacobhandley50@gmail.com>
2018-09-09 19:13:06 -0400

    Updated

--- project1/project1.cc
+++ project1/project1.cc
@@ -17,17 +17,24 @@ int main(){
 	//input is given, the program will not continue, so the "Nan" and "Missing
 	//Argument" messages are unnecessary.
 
-	//Initializes list of integers
-	list<int> l = {2};
-	for(int i = 3; i < n; i++){
-		//Adds all integers in the specified range to the list
-		l.push_back(i);
+	//Mark all integers as true initially
+	bool isprime[n+1] = {true};
+	//Set 2 as the first value of p
+	for(int p = 2; p*p < n; p++){
+		if(isprime[p]){
+			for(int i = p*2; i <= n; i+=p){
+				isprime[i] = false;
+			}
+			cout<<"test"<<endl;
+		}
+
 	}
-	list<int> primes;
-	list<int> iterator it = l.begin();
-	while(l.begin() < sqrt(n)){
-		
+	cout<<"Primes: ";
+	for(int i = 2; i <=n; i++){
+		if(isprime[i])
+			cout<<i<<" ";
 	}
+	cout<<endl;
 
 	return 0;
 }
