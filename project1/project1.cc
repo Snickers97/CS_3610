17aac8157adbbd39f09fa797a5ce7cc62de6e841
Jacob <jacobhandley50@gmail.com>
2018-09-09 20:58:51 -0400

    Pretty much done, except 15 doesn't work :(

--- project1/project1.cc
+++ project1/project1.cc
@@ -1,11 +1,14 @@
 #include <iostream>
-#include <list>
 
 using namespace std;
 
+/*I found it easier to implement the sieve without using a list structure, and
+instead just using a bool array and the loop counter to mark which numbers are
+prime and composite*/
+
 int main(){
-	//Message to the user
 	int n;
+	//Message to the user
 	cout<<"Enter an integer between 2 and 30,000 as your upper bound."<<endl;
 	cin>>n;
 	//Check if input is within bounds
@@ -18,20 +21,30 @@ int main(){
 	//Argument" messages are unnecessary.
 
 	//Mark all integers as true initially
-	bool isprime[n+1] = {true};
+	bool isprime[n+1];
+	for(int i = 0; i < n+2; i++){
+		isprime[i] = true;
+	}
 	//Set 2 as the first value of p
 	for(int p = 2; p*p < n; p++){
 		if(isprime[p]){
+			//For each prime, iterate through array and mark multiples of p as
+			//composite
 			for(int i = p*2; i <= n; i+=p){
 				isprime[i] = false;
 			}
-			cout<<"test"<<endl;
 		}
-
 	}
+	//Print all primes
 	cout<<"Primes: ";
 	for(int i = 2; i <=n; i++){
 		if(isprime[i])
+			cout<<i<<" ";
+	}
+	//Print all composites
+	cout<<endl<<"Composites: ";
+	for(int i = 2; i <=n; i++){
+		if(!isprime[i])
 			cout<<i<<" ";
 	}
 	cout<<endl;
